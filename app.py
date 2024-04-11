e3d7dd907fdd0a4ef1cb52546b269d70f73f57bf
TMIRT <166178660+TMIRT@users.noreply.github.com>
2024-04-11 00:52:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'DevOps CI/CD is AWESOME!!!'
+    return 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
