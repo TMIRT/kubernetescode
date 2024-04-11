5b7dcdf6709568fa4d00c9f8bdd659cd903f4734
TMIRT <166178660+TMIRT@users.noreply.github.com>
2024-04-11 12:23:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+    return 'Hello! Welcome to DevOps CI/CD Pipeline'
