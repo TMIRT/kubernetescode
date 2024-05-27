7e96f42cc031f3a7b968cb258d4abc25d618a14f
TMIRT <166178660+TMIRT@users.noreply.github.com>
2024-05-26 21:59:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello! Welcome to DevOps CI/CD Pipeline. This is build image revision #24'
+    return 'Hello! Welcome to DevOps CI/CD Pipeline. This is build image revision #25'
