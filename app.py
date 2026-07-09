ca7b5a6f4cd0591aed1c38f2ecd7db8a07750e8d
Aldrin Ceriola <aldrin_ceriola@trendmicro.com>
2026-07-09 15:14:31 -0500

    chore: bump version to 1.1.1 (webhook test)

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 START_TIME = datetime.now(timezone.utc)
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 
 def uptime():
     delta = datetime.now(timezone.utc) - START_TIME
